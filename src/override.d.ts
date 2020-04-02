1c221f2f8738f6d49a0ccba7a65955c2527ecf50
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-04-02 17:20:09 -0500

    :pencil: added readme. :sparkles: updated prettier

--- src/override.d.ts
+++ src/override.d.ts
@@ -4,7 +4,7 @@
  * Author: Alex Chomiak
  *
  * Last Modified: Friday, 3rd January 2020 11:09:47 am
- * Modified By: Alex Chomiak 
+ * Modified By: Alex Chomiak
  *
  * Author Github: https://github.com/alexchomiak
  */
